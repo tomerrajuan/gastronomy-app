ba1ed8eb367edf15e10511b7d85751af44b567c9
Nadav Kirsh <saturdaysyouth@gmail.com>
2021-10-07 12:57:43 +0200

    [GMA-24]_create_input_atom

---
+++ src/components/atom/Input.tsx
@@ -0,0 +1,37 @@
+import React, { ReactElement } from "react";
+
+interface Props {
+  className: string;
+  label?: string;
+  placeholder: string;
+  name: string;
+  type?: string;
+  required: boolean;
+  disabled?: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function Input({
+  className,
+  label,
+  placeholder,
+  name,
+  type = "text",
+  disabled = false,
+  required,
+  onChange,
+}: Props): ReactElement {
+  return (
+    <input
+      className={className}
+      placeholder={placeholder}
+      name={name}
+      type={type}
+      required={required}
+      onChange={onChange}
+      disabled={disabled}
+    >
+      <label className="inp__label">{label}</label>
+    </input>
+  );
+}
