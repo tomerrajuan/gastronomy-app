3005a94f7a4b77b913afdd839be9c58d2058416b
tomer <tomer.rajuan@gmail.com>
2021-12-26 23:21:12 +0100

    [GMA-28] add id to td

--- src/components/molecules/TableRow.tsx
+++ src/components/molecules/TableRow.tsx
@@ -8,7 +8,7 @@ export default function TableRow({ rowItem, trClass, tdClass }: Props) {
   return (
     <tr className={trClass} {...rowItem.getRowProps()}>
       {rowItem.cells.map((cell: any) => (
-        <td className={tdClass} {...cell.getCellProps()}>
+        <td key={cell.id} className={tdClass} {...cell.getCellProps()}>
           {cell.render("Cell")}
         </td>
       ))}
