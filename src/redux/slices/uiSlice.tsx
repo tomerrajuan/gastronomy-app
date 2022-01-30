219c225533b956950b09406d6c1720265d59e583
tomer <tomer.rajuan@gmail.com>
2022-01-30 22:35:42 +0100

    [GMA-57] export dispatch and select

--- src/redux/slices/uiSlice.tsx
+++ src/redux/slices/uiSlice.tsx
@@ -21,4 +21,16 @@ const uiSlice = createSlice({
   },
 });
 
+// esport to use with "dispatch" from the components to change the value
+export const setIsAddItemForm =
+  (isAddItemForm: boolean): AppThunk =>
+  async (dispatch: AppDispatch) => {
+    dispatch(uiSlice.actions.setIsAddItemForm({ isAddItemForm }));
+  };
+
+// export to use the global isAddItemForm state in the components
+export const SelectIsAddItemForm = (state: RootState) => {
+  return state.ui.isAddItemForm;
+};
+
 export default uiSlice.reducer;
